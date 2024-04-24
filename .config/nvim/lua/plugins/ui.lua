7edcf34be8002e776790785c5d6bd08e36348c3c
Justin Bacher <32350542+JustinBacher@users.noreply.github.com>
2024-04-24 10:13:42 -0400

    Working on Ollama support

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -11,6 +11,94 @@ local lualine_colors = {
 -- Maybe add https://github.com/stevearc/dressing.nvim if it's useful
 
 return {
+  --    _                                       _             
+  --    ___    __| |   __ _   _   _       _ __   __   __ (_)  _ __ ___  
+  --   / _ \  / _` |  / _` | | | | |     | '_ \  \ \ / / | | | '_ ` _ \ 
+  --  |  __/ | (_| | | (_| | | |_| |  _  | | | |  \ V /  | | | | | | | |
+  --   \___|  \__,_|  \__, |  \__, | (_) |_| |_|   \_/   |_| |_| |_| |_|
+  --                  |___/   |___/                                     
+  {
+    "folke/edgy.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.opt.laststatus = 3
+      vim.opt.splitkeep = "screen"
+    end,
+    opts = {
+      bottom = {
+        -- toggleterm / lazyterm at the bottom with a height of 40% of the screen
+        {
+          ft = "toggleterm",
+          size = { height = 0.4 },
+          -- exclude floating windows
+          filter = function(buf, win)
+            return vim.api.nvim_win_get_config(win).relative == ""
+          end,
+        },
+        {
+          ft = "lazyterm",
+          title = "LazyTerm",
+          size = { height = 0.4 },
+          filter = function(buf)
+            return not vim.b[buf].lazyterm_cmd
+          end,
+        },
+        "Trouble",
+        { ft = "qf", title = "QuickFix" },
+        {
+          ft = "help",
+          size = { height = 20 },
+          -- only show help buffers
+          filter = function(buf)
+            return vim.bo[buf].buftype == "help"
+          end,
+        },
+        { ft = "spectre_panel", size = { height = 0.4 } },
+      },
+      left = {
+        -- Neo-tree filesystem always takes half the screen height
+        {
+          title = "Neo-Tree",
+          ft = "neo-tree",
+          filter = function(buf)
+            return vim.b[buf].neo_tree_source == "filesystem"
+          end,
+          size = { height = 0.5 },
+        },
+        {
+          title = "Neo-Tree Git",
+          ft = "neo-tree",
+          filter = function(buf)
+            return vim.b[buf].neo_tree_source == "git_status"
+          end,
+          pinned = true,
+          open = "Neotree position=right git_status",
+        },
+        {
+          title = "Neo-Tree Buffers",
+          ft = "neo-tree",
+          filter = function(buf)
+            return vim.b[buf].neo_tree_source == "buffers"
+          end,
+          pinned = true,
+          open = "Neotree position=top buffers",
+        },
+        {
+          ft = "Outline",
+          pinned = true,
+          open = "SymbolsOutlineOpen",
+        },
+        -- any other neo-tree windows
+        "neo-tree",
+      },
+      right = {
+        { ft = "codecompanion", title = "Code Companion Chat", size = { width = 0.45 } },
+      },
+      
+      exit_when_last = false,
+    },
+  }
+
     --    _   _                       _             
     --    __   __   ___  (_) | |      _ __   __   __ (_)  _ __ ___  
     --    \ \ / /  / _ \ | | | |     | '_ \  \ \ / / | | | '_ ` _ \ 
