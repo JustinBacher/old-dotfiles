66723927be2738216b435d62266eaeed0e59f521
JustinBacher <92jbach@gmail.com>
2024-05-10 19:23:20 -0400

    removing starship in favor of tide for the async speed

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,3 @@
-function starship_transient_prompt_func
-    starship module character
-end
-
-function starship_transient_rprompt_func
-    starship module cmd_duration
-end
-
-if status is-interactive
-    # abbr --add cdd "cd $(find * -type d | fzf)"
-end
-
-starship init fish | source
 zoxide init --cmd cd fish | source
 set -gx EDITOR nvim
 
@@ -18,5 +5,3 @@ if status is-interactive
     and not set -q TMUX
     exec tmux
 end
-
-enable_transience
