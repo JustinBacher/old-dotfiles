d64698f704337e7109d0014a83075ebfc8d57ef5
JustinBacher <92jbach@gmail.com>
2024-05-11 12:06:40 -0400

    the local leader is in insert mode?

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.maplocalleader = "\\"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.termguicolors = true
 vim.opt.relativenumber = true
 vim.cmd("au ColorScheme * hi Comment cterm=italic gui=italic") -- This fixed the italics not showing up for some reason
