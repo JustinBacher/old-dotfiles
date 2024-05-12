65bf929cb6cca86cf30f8f0e93eb4d257a9517cb
JustinBacher <92jbach@gmail.com>
2024-05-12 14:37:31 -0400

    git stuff let's goooo

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,3 +1,13 @@
 return {
-	"lewis6991/gitsigns.nvim",
+	{
+		"tpope/vim-fugitive",
+		keys = {},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "VeryLazy",
+		keys = {
+			{ "<leader>gp", "<cmd>GitSigns preview_hunk<cr>", desc = "Preview Hunk" },
+		},
+	},
 }
