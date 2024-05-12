b55a68af033b0e2812df90cb0077475bf89d6d0b
JustinBacher <92jbach@gmail.com>
2024-05-12 14:38:06 -0400

    fixing keys for harpoon

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -4,11 +4,11 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     event = "VeryLazy",
     keys = {
-      { "<leader>a", require("harpoon.mark").add_file, desc = "Harpoon: Mark File" },
-      { "<C-e>", require("harpoon.ui").toggle_quick_menu, desc = "Toggle Harpoon Menu" },
-      { "<C-t>", function() require("harpoon.ui").nav_file(1) end, desc = "Harpoon File 1" },
-      { "<C-s>", function() require("harpoon.ui").nav_file(2) end, desc = "Harpoon File 2" },
-      { "<C-b>", function() require("harpoon.ui").nav_file(3) end, desc = "Harpoon File 3" },
-      { "<C-g>", function() require("harpoon.ui").nav_file(4) end, desc = "Harpoon File 4" },
+      { "<leader>a", function() require("harpoon"):list():add() end, desc = "Harpoon: Mark File" },
+      { "<C-e>", function() local h = require("harpoon") h.ui:toggle_quick_menu(h:list()) end, desc = "Toggle Harpoon Menu" },
+      { "<C-t>", function() require("harpoon"):list():select(1) end, desc = "Harpoon File 1" },
+      { "<C-s>", function() require("harpoon"):list():select(2) end, desc = "Harpoon File 2" },
+      { "<C-b>", function() require("harpoon"):list():select(3) end, desc = "Harpoon File 3" },
+      { "<C-g>", function() require("harpoon"):list():select(4) end, desc = "Harpoon File 4" },
   }
 }
