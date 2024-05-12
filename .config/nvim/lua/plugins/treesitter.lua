61c731c377c3aa84960f760707800c425bbaa3bc
JustinBacher <92jbach@gmail.com>
2024-05-12 14:39:28 -0400

    lsp rework

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,14 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "html", "hyprlang" },
-          sync_install = true,
-          highlight = { enable = true },
-          indent = { enable = true },
-        })
-    end
-    vim.filetype.add({
-      pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
-    })
- }
+		configs.setup({
+			ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "html" },
+			sync_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
+}
