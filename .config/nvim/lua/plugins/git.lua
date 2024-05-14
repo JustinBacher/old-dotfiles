5b95ed3d1d0aebbcf1417739da2d64e99d06c050
Justin Bacher <32350542+JustinBacher@users.noreply.github.com>
2024-05-14 11:23:52 -0400

    All git stuff in one file

---
+++ .config/nvim/lua/plugins/git.lua
@@ -0,0 +1,30 @@
+return {
+	{
+		"tpope/vim-fugitive",
+		keys = {},
+		config = true,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "VeryLazy",
+		keys = {
+			{ "<leader>gp", "<cmd>GitSigns preview_hunk<cr>", desc = "Preview Hunk" },
+		},
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+	}
+}
