e1913c5bff6ac9e95dee4f10362e379526b87938
Justin Bacher <32350542+JustinBacher@users.noreply.github.com>
2024-05-14 12:16:37 -0400

    I think this is how I'd get images previewable?

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,17 +1,43 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	opts = {},
-	keys = {
-		{ "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files (Root Dir)" },
-		{ "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Grep (Root Dir)" },
-		{ "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-		{ "<leader>b", "<cmd>Telescope buffers<cr>", desc = "Telescope Buffers" },
-		{
-			"<leader>ff",
-			function()
-				require("telescope.builtin").find_files({ cwd = require("telescope.utils").buffer_dir() })
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {}
+		opts = {},
+		keys = {
+			{ "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files (Root Dir)" },
+			{ "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Grep (Root Dir)" },
+			{ "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+			{ "<leader>b", "<cmd>Telescope buffers<cr>", desc = "Telescope Buffers" },
+			{
+				"<leader>ff",
+				function()
+					require("telescope.builtin").find_files({ cwd = require("telescope.utils").buffer_dir() })
+				end,
+				desc = "Find Files (cwd)",
+			},
+			build = function()
+				require('telescope').load_extension('media_files')
 			end,
-			desc = "Find Files (cwd)",
+		},
+	},
+	{
+		"nvim-telescope/telescope-media-files.nvim",
+		dependencies = {
+			"nvim-lua/popup.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		lazy = true,
+		opts = {
+			extensions = {
+				media_files = {
+				  -- filetypes whitelist
+				  -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+				  filetypes = {"png", "webp", "jpg", "jpeg"},
+				  -- find command (defaults to `fd`)
+				  find_cmd = "rg"
+				}
+			  },
 		},
 	},
 }
