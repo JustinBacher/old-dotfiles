fb46c5ad2ba8cca322c13074c79981efb483077c
Justin Bacher <32350542+JustinBacher@users.noreply.github.com>
2024-05-14 11:48:11 -0400

    Formatting and code slimming

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,5 +1,5 @@
-local HEIGHT_RATIO = 0.8 -- You can change this
-local WIDTH_RATIO = 0.5 -- You can change this took
+local HEIGHT_RATIO = 0.8
+local WIDTH_RATIO = 0.5
 
 return {
 	"nvim-tree/nvim-tree.lua",
@@ -39,20 +39,15 @@ return {
 				enable = true,
 				open_win_config = function()
 					local screen_w = vim.opt.columns:get()
-					local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
 					local window_w = screen_w * WIDTH_RATIO
-					local window_h = screen_h * HEIGHT_RATIO
-					local window_w_int = math.floor(window_w)
-					local window_h_int = math.floor(window_h)
-					local center_x = (screen_w - window_w) / 2
-					local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+					local window_h = (vim.opt.lines:get() - vim.opt.cmdheight:get()) * HEIGHT_RATIO
 					return {
 						border = "rounded",
 						relative = "editor",
-						row = center_y,
-						col = center_x,
-						width = window_w_int,
-						height = window_h_int,
+						row = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get(),
+						col = (screen_w - window_w) / 2,
+						width = math.floor(window_w),
+						height = math.floor(window_h),
 					}
 				end,
 			},
