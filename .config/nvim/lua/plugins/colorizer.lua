7859f5a71fed536e85358bb4b05e6044c5ffa056
Justin Bacher <32350542+JustinBacher@users.noreply.github.com>
2024-05-14 11:19:40 -0400

    Telescope already does this

--- .config/nvim/lua/plugins/colorizer.lua
+++ .config/nvim/lua/plugins/colorizer.lua
@@ -1,11 +1,9 @@
 return {
 	"brenoprata10/nvim-highlight-colors",
 	event = "VeryLazy",
-	config = function()
-		require("nvim-highlight-colors").setup({
-			render = "virtual",
-			virtual_symbol = "",
-			enable_tailwind = true,
-		})
-	end,
+	opts = {
+		render = "virtual",
+		virtual_symbol = "",
+		enable_tailwind = true,
+	}
 }
