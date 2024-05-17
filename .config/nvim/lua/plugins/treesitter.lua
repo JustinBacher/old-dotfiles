4427cf2e5c25ee002abd6ffd308b49193ea26d61
JustinBacher <92jbach@gmail.com>
2024-05-17 19:19:56 -0400

    i swear this whole lsp and ts thing seems hard now but i will get this

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,27 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	main = "nvim-treesitter.configs",
-	opts = {
-		ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "html" },
-		sync_install = true,
-		highlight = { enable = true },
-		indent = { enable = true },
-	},
+		event = "VeryLazy",
+		build = ":TSUpdate",
+		init = function()
+			vim.api.nvim_create_autocmd('BufReadPost', {
+					group = vim.api.nvim_create_augroup('treesitter', {}),
+					callback = function(ev)
+					local max_filesize = 100 * 1024 -- 100 KB
+					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(ev.buf))
+					if ok and stats and stats.size > max_filesize then
+						return true
+					end
+					vim.treesitter.start(ev.buf)
+				end,
+		})
+	end,
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+			sync_install = false,
+			auto_install = true,
+			highlight = { enabled = false },
+			additional_vim_regex_highlighting = false,
+		})
+	end,
 }
