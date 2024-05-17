fb7aacc3274126d81dfb0444d5856d9654b1155c
JustinBacher <92jbach@gmail.com>
2024-05-17 19:16:21 -0400

    lazy colorschemes!!

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -3,14 +3,12 @@ return {
 	-- TODO: Figure out if I can turn lazy to true for all but current theme
 	{
 		"Mofiqul/dracula.nvim",
-		lazy = false,
 		name = "dracula",
 		priority = 1000,
 		opts = { transparent_bg = true, italic_comment = true },
 	},
 	{
 		"folke/tokyonight.nvim",
-		lazy = false,
 		name = "tokyonight",
 		priority = 1000,
 		opts = {
@@ -23,14 +21,12 @@ return {
 	},
 	{
 		"catppuccin/nvim",
-		lazy = false,
 		name = "catppuccin",
 		priority = 1000,
 		opts = { transparent_background = true },
 	},
 	{
 		"rose-pine/neovim",
-		lazy = false,
 		name = "rose-pine",
 		priority = 1000,
 		opts = { extend_background_behind_borders = false, styles = { transparency = true } },
@@ -47,14 +43,14 @@ return {
 			end
 			themery.setup({
 				themes = {
-					{ name = "Dracula Dark", colorscheme = "dracula" },
-					{ name = "Dracula Soft", colorscheme = "dracula-soft" },
-					{ name = "Tokyonight Storm", colorscheme = "tokyonight-night" },
-					{ name = "Tokyonight Night", colorscheme = "tokyonight-night" },
-					{ name = "Tokyonight Moon", colorscheme = "tokyonight-moon" },
-					{ name = "Catppuccin Mocha", colorscheme = "catppuccin-mocha" },
+					{ name = "Dracula Dark",         colorscheme = "dracula" },
+					{ name = "Dracula Soft",         colorscheme = "dracula-soft" },
+					{ name = "Tokyonight Storm",     colorscheme = "tokyonight-night" },
+					{ name = "Tokyonight Night",     colorscheme = "tokyonight-night" },
+					{ name = "Tokyonight Moon",      colorscheme = "tokyonight-moon" },
+					{ name = "Catppuccin Mocha",     colorscheme = "catppuccin-mocha" },
 					{ name = "Catppuccin Macchiato", colorscheme = "catppuccin-macchiato" },
-					{ name = "Rose Pine Moon", colorscheme = "rose-pine-moon" },
+					{ name = "Rose Pine Moon",       colorscheme = "rose-pine-moon" },
 				},
 				themeConfigFile = "~/dotfiles/.config/nvim/lua/config/theme.lua",
 				livePreview = true,
@@ -73,5 +69,5 @@ return {
 			virtual_symbol = "",
 			enable_tailwind = true,
 		},
-	},	
+	},
 }
