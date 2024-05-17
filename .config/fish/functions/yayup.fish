7ad771a0d60453933fceb75bc52d17eaf9da6697
JustinBacher <92jbach@gmail.com>
2024-05-17 19:13:15 -0400

    for wife to be able to keep up to date while on deployment

---
+++ .config/fish/functions/yayup.fish
@@ -0,0 +1,4 @@
+function yayup --wraps='yay -Syu --noconfirm || shutdown now' --description 'alias yayup yay -Syu --noconfirm || shutdown now'
+  yay -Syu --noconfirm --sudoloop --skipreview 
+        
+end
