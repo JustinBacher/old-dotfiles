a385c09f7cbc5caaca589a50e7d14bb2cdc0e332
JustinBacher <92jbach@gmail.com>
2024-05-17 19:15:51 -0400

    i think im gonna redo this but using no remap now that i understand what it does

--- .config/nvim/lua/config/remaps.lua
+++ .config/nvim/lua/config/remaps.lua
@@ -6,6 +6,8 @@ local diagnostic_goto = function(next, severity)
   end
 end
 
+NOT_INSERT = { "n", "x", "v", "o" }
+
 return {
 	-- Global ease of use
 	{ "<leader>qq", "<cmd>qa<cr>", desc = "Quit All Buffers" },
@@ -14,38 +16,32 @@ return {
 	{ "<leader>w", "<cmd>w<cr><esc>", desc = "Write file" },
 
 	-- Movement
-	{ "J", "<C-d>zz", desc = "Move up half page" },
-	{ "K", "<C-u>zz", desc = "Move up half page" },
-	{ "H", "^", desc = "Easier goto beginning of line" },
-	{ "L", "$", desc = "Easier goto end of line" },
+	{ "J", "<C-d>zz", mode = NOT_INSERT, noremap = true, desc = "Move up half page" },
+	{ "K", "<C-u>zz", mode = NOT_INSERT, noremap = true, desc = "Move up half page" },
+	{ "H", "^", mode = NOT_INSERT, noremap = true, desc = "Easier goto beginning of line" },
+	{ "L", "$", mode = NOT_INSERT, noremap = true, desc = "Easier goto end of line" },
 
 	-- Moving lines
-	{ "<A-J>", "<cmd>m .+1<cr>==", desc = "Move Line Down" },
-	{ "<A-K>", "<cmd>m .-2<cr>==", desc = "Move Line Up" },
-	{ "<A-J>", "<esc><cmd>m .+1<cr>==gi", mode = "i", desc = "Move Line Down" },
-	{ "<A-K>", "<esc><cmd>m .-2<cr>==gi", mode = "i", desc = "Move Line Up" },
-	{ "<A-J>", ":m '>+1<cr>gv=gv", mode = "v", desc = "Move Line Down" },
-	{ "<A-K>", ":m '<-2<cr>gv=gv", mode = "v", desc = "Move Line Up" },
+	{ "<A-J>", "<cmd>m .+1<cr>==", noremap = true, desc = "Move Line Down" },
+	{ "<A-K>", "<cmd>m .-2<cr>==", noremap = true, desc = "Move Line Up" },
+	{ "<A-J>", "<cmd>m '>+1<CR>gv=gv", mode = "v", noremap = true, desc = "Move Line Down" },
+	{ "<A-K>", "<cmd>m '<-2<CR>gv=gv", mode = "v", noremap = true, desc = "Move Line Up" },
 
 	-- Tabs
-	{ "<Tab>", "<cmd>bnext<cr>", desc = "Next Tab" },
- { "<S-Tab>", "<cmd>bprevious<cr>", desc = "Previous Tab" },
+	{ "<Tab>", "<cmd>bnext<cr>", mode = NOT_INSERT, desc = "Next Tab" },
+	{ "<S-Tab>", "<cmd>bprevious<cr>", mode = NOT_INSERT, desc = "Previous Tab" },
 
--- Windows
- { "<leader>wd", "<C-W>c", desc = "Delete Window", remap = true },
- { "<leader>wj", "<C-W>s", desc = "Split Window Below", remap = true },
- { "<leader>wl", "<C-W>v", desc = "Split Window Right", remap = true },
- { "<leader>wrh", "<cmd>vertical resize -5", desc = "Resize Window Up" },
- { "<leader>wrj", "<cmd>resize +5", desc = "Resize Window Down" },
-
--- Diagnostics
- { "<leader>dd", vim.diagnostic.open_float, desc = "Line Diagnostics" },
- { "<leader>dj", diagnostic_goto(true), desc = "Next Diagnostic" },
- { "<leader>dk", diagnostic_goto(false), desc = "Prev Diagnostic" },
+	-- Windows
+	{ "<leader>wd", "<C-w>c", mode = NOT_INSERT, desc = "Delete Window" },
+	{ "<leader>wj", "<C-w>s", mode = NOT_INSERT, desc = "Split Window Below" },
+	{ "<leader>wl", "<C-w>v", mode = NOT_INSERT, desc = "Split Window Right" },
+	{ "<leader>wrh", "<Cmd>vertical resize -5", desc = "Resize Window Up" },
+	{ "<leader>wrj", "<Cmd>resize +5", desc = "Resize Window Down" },
 
 	-- Miscellaneous
- { "<leader>ll", "<cmd>lopen<cr>", desc = "Location List" },
- { "<leader>qf", "<cmd>copen<cr>", desc = "Quickfix List" },
+	{ "<leader>ll", "<cmd>lopen<cr>", desc = "Location List" },
+	{ "<leader>qf", "<cmd>copen<cr>", desc = "Quickfix List" },
 	{ "U", "<C-r>", desc = "Redo" },
 	{ "<esc>", "<cmd>noh<cr><esc>", desc = "Escape and Clear hlsearch" },
+	{ "gj", "J", mode = NOT_INSERT, noremap = true, desc = "join line below to the current one with one space in between (remap of J)" }
 }
