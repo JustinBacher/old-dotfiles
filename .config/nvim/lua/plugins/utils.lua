d204fe0b16c9573ef724bf3134cdb268bdc21612
Justin Bacher <32350542+JustinBacher@users.noreply.github.com>
2024-05-17 10:50:14 -0400

    Fix Telescope not loading if other plugin and dial & karen not loading at all

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -1,11 +1,12 @@
 return {
     {
         "monaqa/dial.nvim",
+        event = "LazyFile",
         config = function()
             -- Don't know why I need to specify keymaps like this and not as lazy keys but meh whatever
             local dial_map = require("dial.map")
             local map = vim.keymap.set
-            local opts = { silent = true }
+            local opts = { noremap = true, silent = true }
             map("n", "<C-a>", dial_map.inc_normal(), opts)
             map("n", "<C-x>", dial_map.dec_normal(), opts)
             map("x", "<C-a>", dial_map.inc_visual(), opts)
@@ -43,6 +44,7 @@ return {
       },
       {
 		"nvim-telescope/telescope.nvim",
+        cmd = "Telescope",
 		dependencies = { "nvim-telescope/telescope-media-files.nvim" }
 		opts = {},
 		keys = {
@@ -64,7 +66,6 @@ return {
 	},
 	{
 		"nvim-telescope/telescope-media-files.nvim",
-		lazy = true,
 		dependencies = {
 			"nvim-lua/popup.nvim",
 			"nvim-lua/plenary.nvim",
@@ -82,5 +83,5 @@ return {
 			  },
 		},
 	},
-    { "tenxsoydev/karen-yank.nvim", config = true },
+    { "tenxsoydev/karen-yank.nvim", event = "LazyFile", config = true },
 }
