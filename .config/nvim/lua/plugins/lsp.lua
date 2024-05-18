1b32c2ff1e3c5352f38c4c018be0560413ed3730
JustinBacher <92jbach@gmail.com>
2024-05-18 04:18:02 -0400

    lsp junk

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -5,7 +5,7 @@ return {
 			"williamboman/mason-lspconfig.nvim",
 			"b0o/schemastore.nvim",
 			"hrsh7th/nvim-cmp",
-			{ "https://git.sr.ht/~whynothugo/lsp_lines.nvim", name = "lsp_lines" },
+			{ "https://git.sr.ht/~whynothugo/lsp_lines.nvim", version = false },
 			{ "folke/neodev.nvim", config = true },
 			{ "j-hui/fidget.nvim", config = true },
 		},
@@ -147,9 +147,7 @@ return {
 			end
 
 			local present_mason, mason = pcall(require, "mason-lspconfig")
-			if present_mason then
-				mason.setup({ ensure_installed = server_names })
-			end
+			if present_mason then mason.setup({ ensure_installed = server_names }) end
 
 			for server_name, server_config in pairs(servers) do
 				local merged_config = vim.tbl_deep_extend("force", default_lsp_config, server_config)
@@ -157,9 +155,7 @@ return {
 
 				if server_name == "rust_analyzer" then
 					local present_rust_tools, rust_tools = pcall(require, "rust-tools")
-					if present_rust_tools then
-						rust_tools.setup({ server = merged_config })
-					end
+					if present_rust_tools then rust_tools.setup({ server = merged_config }) end
 				end
 			end
 		end,
@@ -182,9 +178,7 @@ return {
 		keys = {
 			{
 				"<leader>bf",
-				function()
-					require("conform").format({ async = true, lsp_fallback = true })
-				end,
+				function() require("conform").format({ async = true, lsp_fallback = true }) end,
 				desc = "Format buffer",
 				mode = { "n", "v" },
 			},
@@ -202,24 +196,18 @@ return {
 		keys = {
 			{
 				"zR",
-				function()
-					require("ufo").openAllFolds()
-				end,
+				function() require("ufo").openAllFolds() end,
 				desc = "Open all folds",
 			},
 			{
 				"zM",
-				function()
-					require("ufo").closeAllFolds()
-				end,
+				function() require("ufo").closeAllFolds() end,
 				desc = "Close all folds",
 			},
 			{
 				"zK",
 				function()
-					if not require("ufo").peekFoldedLinesUnderCursor() then
-						vim.lsp.buf.hover()
-					end
+					if not require("ufo").peekFoldedLinesUnderCursor() then vim.lsp.buf.hover() end
 				end,
 				desc = "Peek Fold",
 			},
@@ -233,6 +221,7 @@ return {
 	-- Completion
 	{
 		"hrsh7th/nvim-cmp",
+		version = false,
 		event = "LazyFile",
 		dependencies = {
 			"hrsh7th/cmp-nvim-lsp-signature-help",
@@ -258,21 +247,18 @@ return {
 						winhighlight = "Normal:Normal,FloatBorder:LspBorderBG,CursorLine:PmenuSel,Search:None",
 					}),
 				},
-				---@diagnostic disable-next-line
 				view = {
 					entries = "bordered",
 				},
 				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
+					expand = function(args) require("luasnip").lsp_expand(args.body) end,
 				},
 				formatting = {
 					format = require("lspkind").cmp_format({
 						mode = "symbol", -- show only symbol annotations
-						maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+						-- maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
 						-- can also be a function to dynamically calculate max width such as
-						-- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+						maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
 						ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
 						show_labelDetails = true, -- show labelDetails in menu. Disabled by default
 
@@ -370,30 +356,22 @@ return {
 			{ "go", "<cmd>Lspsaga outline<cr>", desc = "Show outline" },
 			{
 				"[e",
-				function()
-					require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
-				end,
+				function() require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR }) end,
 				desc = "Jump to previous error",
 			},
 			{
 				"]e",
-				function()
-					require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
-				end,
+				function() require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR }) end,
 				desc = "Jump to next error",
 			},
 			{
 				"[w",
-				function()
-					require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.WARN })
-				end,
+				function() require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.WARN }) end,
 				desc = "Jump to previous warning",
 			},
 			{
 				"]w",
-				function()
-					require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.WARN })
-				end,
+				function() require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.WARN }) end,
 				desc = "Jump to next warning",
 			},
 			{
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,21 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-		event = "VeryLazy",
-		build = ":TSUpdate",
-		init = function()
-			vim.api.nvim_create_autocmd('BufReadPost', {
-					group = vim.api.nvim_create_augroup('treesitter', {}),
-					callback = function(ev)
-					local max_filesize = 100 * 1024 -- 100 KB
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(ev.buf))
-					if ok and stats and stats.size > max_filesize then
-						return true
-					end
-					vim.treesitter.start(ev.buf)
-				end,
-		})
-	end,
+	version = "*",
+	event = "LazyFile",
+	build = ":TSUpdate",
 	config = function()
+		--- @diagnostic disable-next-line: missing-fields
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
 			sync_install = false,
-			auto_install = true,
+			-- indent = { enable = true },
 			highlight = { enabled = false },
 			additional_vim_regex_highlighting = false,
+			disable = function(lang, buf)
+				local max_filesize = 100 * 1024 -- 100 KB
+				local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+				if ok and stats and stats.size > max_filesize then return true end
+			end,
 		})
 	end,
 }
