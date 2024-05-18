84a05c750def8b4ac537c5fed99b1379fdfba7ee
JustinBacher <92jbach@gmail.com>
2024-05-18 04:16:06 -0400

    encoding got messed up?

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,35 +1,35 @@
 return {
-    {
-      'williamboman/mason.nvim',
-      cmd = {
-        "Mason",
-        "MasonUpdate",
-        "MasonInstall",
-        "MasonUninstall",
-        "MasonUninstallAll",
-        "MasonLog",
-      },
-      event = "LazyFile",
-      config = true,
-    },
-    {
-      'williamboman/mason-lspconfig.nvim',
-      dependencies = { 'williamboman/mason.nvim' },
-      opts = {
-        ensure_installed = {},
-        inlay_hints = { enabled = true }
-      },
-    },
-    {
-      'jay-babu/mason-nvim-dap.nvim',
-      event = "VeryLazy",
-      dependencies = { 'williamboman/mason.nvim' },
-      opts = {},
-      config = function(_, opts)
-        local mdap = require('mason-nvim-dap')
-        mdap.setup(vim.tbl_deep_extend("force", opts, {
-          handlers = { mdap.default_setup }
-        }))
-      end,
-    },
-  }
+	{
+		"williamboman/mason.nvim",
+		cmd = {
+			"Mason",
+			"MasonUpdate",
+			"MasonInstall",
+			"MasonUninstall",
+			"MasonUninstallAll",
+			"MasonLog",
+		},
+		event = "LazyFile",
+		config = true,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		opts = {
+			ensure_installed = {},
+			inlay_hints = { enabled = true },
+		},
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		opts = {},
+		config = function(_, opts)
+			local mdap = require("mason-nvim-dap")
+			mdap.setup(vim.tbl_deep_extend("force", opts, {
+				handlers = { mdap.default_setup },
+			}))
+		end,
+	},
+}
+
--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -1,52 +1,51 @@
 return {
-    {
-        "monaqa/dial.nvim",
-        event = "LazyFile",
-        config = function()
-            -- Don't know why I need to specify keymaps like this and not as lazy keys but meh whatever
-            local dial_map = require("dial.map")
-            local map = vim.keymap.set
-            local opts = { noremap = true, silent = true }
-            map("n", "<C-a>", dial_map.inc_normal(), opts)
-            map("n", "<C-x>", dial_map.dec_normal(), opts)
-            map("x", "<C-a>", dial_map.inc_visual(), opts)
-            map("x", "<C-x>", dial_map.dec_visual(), opts)
-            map("x", "g<C-a>", dial_map.inc_gvisual(), opts)
-            map("x", "g<C-x>", dial_map.dec_gvisual(), opts)
-    
-            local augend = require("dial.augend")
-            require("dial.config").augends:register_group({
-                default = {
-                    augend.integer.alias.decimal,
-                    augend.integer.alias.hex,
-                    augend.date.alias["%Y/%m/%d"],
-                    augend.date.alias["%Y-%m-%d"],
-                    augend.date.alias["%m/%d"],
-                    augend.date.alias["%H:%M"],
-                    augend.semver.alias.semver,
-                    augend.constant.alias.bool,
-                },
-            })
-        end,
-    },
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        init = function()
-          vim.o.timeout = true
-          vim.o.timeoutlen = 300
-        end,
-        opts = {
-          window = {
-            border = "shadow",
-          },
-        }
-      },
-      {
+	{ "windwp/nvim-autopairs", event = "InsertEnter", config = true },
+	{
+		"monaqa/dial.nvim",
+		event = "LazyFile",
+		config = function()
+			-- Don't know why I need to specify keymaps like this and not as lazy keys but meh whatever
+			local dial_map = require("dial.map")
+			local map = vim.keymap.set
+			local opts = { noremap = true, silent = true }
+			map("n", "<C-a>", dial_map.inc_normal(), opts)
+			map("n", "<C-x>", dial_map.dec_normal(), opts)
+			map("x", "<C-a>", dial_map.inc_visual(), opts)
+			map("x", "<C-x>", dial_map.dec_visual(), opts)
+			map("x", "g<C-a>", dial_map.inc_gvisual(), opts)
+			map("x", "g<C-x>", dial_map.dec_gvisual(), opts)
+
+			local augend = require("dial.augend")
+			require("dial.config").augends:register_group({
+				default = {
+					augend.integer.alias.decimal,
+					augend.integer.alias.hex,
+					augend.date.alias["%Y/%m/%d"],
+					augend.date.alias["%Y-%m-%d"],
+					augend.date.alias["%m/%d"],
+					augend.date.alias["%H:%M"],
+					augend.semver.alias.semver,
+					augend.constant.alias.bool,
+				},
+			})
+		end,
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {
+			window = {
+				border = "shadow",
+			},
+		},
+	},
+	{
 		"nvim-telescope/telescope.nvim",
 		dependencies = { "nvim-telescope/telescope-media-files.nvim" },
-		event = "VeryLazy",
-		opts = {},
 		keys = {
 			{ "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files (Root Dir)" },
 			{ "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Grep (Root Dir)" },
@@ -54,34 +53,16 @@ return {
 			{ "<leader>b", "<cmd>Telescope buffers<cr>", desc = "Telescope Buffers" },
 			{
 				"<leader>ff",
-				function()
-					require("telescope.builtin").find_files({ cwd = require("telescope.utils").buffer_dir() })
-				end,
+				function() require("telescope.builtin").find_files({ cwd = require("telescope.utils").buffer_dir() }) end,
 				desc = "Find Files (cwd)",
 			},
-			build = function()
-				require('telescope').load_extension('media_files')
-			end,
 		},
+		build = function()
+			local l = require("telescope").load_extension
+			l("media_files")
+			l("noice")
+		end,
+		config = true,
 	},
-	{
-		"nvim-telescope/telescope-media-files.nvim",
-		dependencies = {
-			"nvim-lua/popup.nvim",
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope.nvim",
-		},
-		opts = {
-			extensions = {
-				media_files = {
-				  -- filetypes whitelist
-				  -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-				  filetypes = {"png", "webp", "jpg", "jpeg"},
-				  -- find command (defaults to `fd`)
-				  find_cmd = "rg"
-				}
-			  },
-		},
-	},
-    { "tenxsoydev/karen-yank.nvim", event = "LazyFile", config = true },
+	{ "tenxsoydev/karen-yank.nvim", event = "LazyFile", config = true },
 }
