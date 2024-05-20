909d0ac5c46b12b82351eb1b50888d6d76a0ec35
JustinBacher <92jbach@gmail.com>
2024-05-19 21:48:34 -0400

    formatting

--- .config/nvim/lua/plugins/motion.lua
+++ .config/nvim/lua/plugins/motion.lua
@@ -41,28 +41,26 @@ return {
 		},
 	},
 	{
-		{
-			"letieu/harpoon-lualine",
-			dependencies = {
-				{
-					"ThePrimeagen/harpoon",
-					branch = "harpoon2",
-					dependencies = { "nvim-lua/plenary.nvim" },
-					keys = {
-						{ "<leader>a", function() require("harpoon"):list():add() end, desc = "Harpoon: Mark File" },
-						{
-							"<C-e>",
-							function()
-								local h = require("harpoon")
-								h.ui:toggle_quick_menu(h:list())
-							end,
-							desc = "Toggle Harpoon Menu",
-						},
-						{ "<C-t>", function() require("harpoon"):list():select(1) end, desc = "Harpoon File 1" },
-						{ "<C-s>", function() require("harpoon"):list():select(2) end, desc = "Harpoon File 2" },
-						{ "<C-b>", function() require("harpoon"):list():select(3) end, desc = "Harpoon File 3" },
-						{ "<C-g>", function() require("harpoon"):list():select(4) end, desc = "Harpoon File 4" },
+		"letieu/harpoon-lualine",
+		dependencies = {
+			{
+				"ThePrimeagen/harpoon",
+				branch = "harpoon2",
+				dependencies = { "nvim-lua/plenary.nvim" },
+				keys = {
+					{ "<leader>a", function() require("harpoon"):list():add() end, desc = "Harpoon: Mark File" },
+					{
+						"<C-e>",
+						function()
+							local h = require("harpoon")
+							h.ui:toggle_quick_menu(h:list())
+						end,
+						desc = "Toggle Harpoon Menu",
 					},
+					{ "<C-t>", function() require("harpoon"):list():select(1) end, desc = "Harpoon File 1" },
+					{ "<C-s>", function() require("harpoon"):list():select(2) end, desc = "Harpoon File 2" },
+					{ "<C-b>", function() require("harpoon"):list():select(3) end, desc = "Harpoon File 3" },
+					{ "<C-g>", function() require("harpoon"):list():select(4) end, desc = "Harpoon File 4" },
 				},
 			},
 		},
