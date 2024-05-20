f85af2ac7dc75a649c3f456fffb90cdee470d53d
JustinBacher <92jbach@gmail.com>
2024-05-19 21:49:27 -0400

    make lazy if not needed on startup and rework the auto close to be simpler/better for my workflow

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -4,27 +4,21 @@ local WIDTH_RATIO = 0.5
 return {
 	"nvim-tree/nvim-tree.lua",
 	version = false,
-	lazy = false,
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
+	lazy = next(vim.fn.argv()) == nil, ---@diagnostic disable-line: param-type-mismatch
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	init = function()
+		local api = require("nvim-tree.api")
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
-		vim.api.nvim_create_autocmd("BufEnter", {
-			nested = true,
-			callback = function()
-				if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
-					vim.cmd("quit")
-				end
-			end,
-		})
-		vim.api.nvim_create_autocmd({ "VimEnter" }, {
+
+		-- Make tree close if last buffer
+		vim.api.nvim_create_autocmd("QuitPre", { callback = function() vim.cmd("NvimTreeClose") end })
+
+		-- If no editable buffer on open then open the tree
+		vim.api.nvim_create_autocmd("VimEnter", {
 			callback = function(data)
-				local real_file = vim.fn.filereadable(data.file) == 1
-				local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
-				if real_file and no_name then
-					require("nvim-tree.api").tree.toggle({ focus = false, find_file = true })
+				if vim.fn.filereadable(data.file) == 1 and data.file == "" and vim.bo[data.buf].buftype == "" then
+					api.toggle({ focus = false, find_file = true })
 				end
 			end,
 		})
@@ -54,35 +48,37 @@ return {
 		},
 		on_attach = function(bufnr)
 			local api = require("nvim-tree.api")
-
-			local function opts(desc)
-				return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-			end
-
 			api.config.mappings.default_on_attach(bufnr)
 
-			vim.keymap.set("n", "<BS>", api.tree.change_root_to_parent, opts("Up"))
-			vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
-			vim.keymap.set("n", "h", api.tree.close, opts("Close"))
-			vim.keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
-			vim.keymap.set("n", "l", function()
-				local node = api.tree.get_node_under_cursor()
-				if node.nodes ~= nil then
+			local function map(key, op, desc)
+				vim.keymap.set(
+					"n",
+					key,
+					op,
+					{ desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+				)
+			end
+
+			map("<BS>", api.tree.change_root_to_parent, "Up")
+			map("?", api.tree.toggle_help, "Help")
+			map("h", api.tree.close, "Close")
+			map("H", api.tree.collapse_all, "Collapse All")
+			map("l", function()
+				if api.tree.get_node_under_cursor().nodes ~= nil then
 					api.node.open.edit()
 				else
 					api.node.open.edit()
 					api.tree.close()
 				end
-			end, opts("Edit Or Open"))
-			vim.keymap.set("n", "L", function()
-				local node = api.tree.get_node_under_cursor()
-				if node.nodes ~= nil then
+			end, "Edit Or Open")
+			map("L", function()
+				if api.tree.get_node_under_cursor().nodes ~= nil then
 					api.node.open.edit()
 				else
 					api.node.open.vertical()
 				end
 				api.tree.focus()
-			end, opts("Vsplit Preview"))
+			end, "Vsplit Preview")
 		end,
 	},
 }
