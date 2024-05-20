713f57a8031b4a1a5bffffe517b75b8fecc452f4
JustinBacher <92jbach@gmail.com>
2024-05-19 21:50:26 -0400

    i like having as much as i can in opts and not in config as possible plug some other fixes/changes

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -2,24 +2,24 @@ return {
 	{
 		"lukas-reineke/indent-blankline.nvim",
 		main = "ibl",
-		config = function()
-			local highlight = {
-				RainbowRed = "#E06C75",
-				RainbowYellow = "#E5C07B",
-				RainbowBlue = "#61AFEF",
-				RainbowOrange = "#D19A66",
-				RainbowGreen = "#98C379",
-				RainbowViolet = "#C678DD",
-				RainbowCyan = "#56B6C2",
-			}
+		opts = {
+			RainbowRed = "#E06C75",
+			RainbowYellow = "#E5C07B",
+			RainbowBlue = "#61AFEF",
+			RainbowOrange = "#D19A66",
+			RainbowGreen = "#98C379",
+			RainbowViolet = "#C678DD",
+			RainbowCyan = "#56B6C2",
+		},
+		config = function(_, opts)
 			local hooks = require("ibl.hooks")
 			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-				for hl, color in pairs(highlight) do
+				for hl, color in pairs(opts) do
 					vim.api.nvim_set_hl(0, hl, { fg = color })
 				end
 			end)
-			vim.g.rainbow_delimiters = { highlight = highlight }
-			require("ibl").setup({ scope = { highlight = highlight } })
+			vim.g.rainbow_delimiters = { highlight = opts }
+			require("ibl").setup({ scope = { highlight = opts } })
 
 			hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
 		end,
@@ -48,52 +48,16 @@ return {
 			},
 		},
 	},
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
-		opts = {
-			lsp = {
-				hover = { enabled = false },
-				signature = { enabled = false },
-				override = {
-					-- ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					-- ["vim.lsp.util.stylize_markdown"] = true,
-					-- ["config.lsp.signature.enabled"] = true,
-					-- ["cmp.entry.get_documentation"] = true,
-				},
-			},
-			presets = {
-				command_palette = true, -- position the cmdline and popupmenu together
-				long_message_to_split = true, -- long messages will be sent to a split
-				inc_rename = true, -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = true, -- add a border to hover docs and signature help
-			},
-			views = {
-				cmdline_popup = {
-					position = { row = 5, col = "50%" },
-					size = { width = 60, height = "auto" },
-				},
-				popupmenu = {
-					relative = "editor",
-					position = { row = 8, col = "50%" },
-					size = { width = 60, height = 10 },
-					border = { style = "rounded", padding = { 0, 1 } },
-					win_options = { winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" } },
-				},
-			},
-			routes = { { view = "notify", filter = { event = "msg_showmode" } } },
-		},
-	},
 	{ "stevearc/dressing.nvim", config = true },
 	{
 		"nvim-lualine/lualine.nvim",
+		-- version = false,
+		event = "VeryLazy",
 		dependencies = {
 			"nvim-tree/nvim-web-devicons",
 			"letieu/harpoon-lualine",
 			"folke/noice.nvim",
 		},
-		event = "LazyFile",
 		opts = {
 			options = {
 				theme = "tokyonight",
@@ -101,17 +65,20 @@ return {
 				section_separators = {},
 			},
 			sections = {
-				lualine_a = {
+				lualine_a = { "mode" },
+				lualine_b = { "diff", "diagnostics" },
+				lualine_c = { "harpoon2" },
+				lualine_x = {
 					function()
+						local n = require("noice")
 						return {
-							require("noice").api.status.mode.get_hl, --- @diagnostic disable-line: undefined-field
-							cond = require("noice").api.status.mode.has, --- @diagnostic disable-line: undefined-field
+							{ n.api.status.message.get_hl, cond = n.api.status.message.has }, ---@diagnostic disable-line: undefined-field
+							{ n.api.status.command.get, cond = n.api.status.command.has, color = { fg = "#ff9e64" } }, ---@diagnostic disable-line: undefined-field
+							{ n.api.status.mode.get, cond = n.api.status.mode.has, color = { fg = "#ff9e64" } }, ---@diagnostic disable-line: undefined-field
+							{ n.api.status.search.get, cond = n.api.status.search.has, color = { fg = "#ff9e64" } }, ---@diagnostic disable-line: undefined-field
 						}
 					end,
 				},
-				lualine_b = { "diff", "diagnostics" },
-				lualine_c = { "harpoon2" },
-				lualine_x = {},
 				lualine_y = { "filetype", "progress" },
 				lualine_z = { { "location", left_padding = 2 } },
 			},
@@ -129,7 +96,8 @@ return {
 	},
 	{
 		"goolord/alpha-nvim",
-		lazy = false,
+		---@diagnostic disable-next-line: param-type-mismatch
+		lazy = next(vim.fn.argv()) == nil,
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function() require("alpha").setup(require("alpha.themes.dashboard").config) end,
 	},
@@ -158,6 +126,7 @@ return {
 		"akinsho/bufferline.nvim",
 		version = false,
 		dependencies = "nvim-tree/nvim-web-devicons",
+		event = "LazyFile",
 		opts = {
 			options = {
 				diagnostics = "nvim_lsp",
@@ -195,13 +164,15 @@ return {
 					["config.lsp.signature.enabled"] = true,
 					-- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 				},
+				signature = { enabled = false },
+				hover = { enabled = false },
 			},
 			-- you can enable a preset for easier configuration
 			presets = {
 				command_palette = true, -- position the cmdline and popupmenu together
 				long_message_to_split = true, -- long messages will be sent to a split
-				inc_rename = false, -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = false, -- add a border to hover docs and signature help
+				inc_rename = true, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = true, -- add a border to hover docs and signature help
 			},
 			views = {
 				cmdline_popup = {
@@ -218,44 +189,6 @@ return {
 			},
 			routes = { { view = "notify", filter = { event = "msg_showmode" } } },
 		},
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-			{ "letieu/harpoon-lualine", dependencies = { "ThePrimeagen/harpoon" } },
-		},
-		opts = {
-			options = {
-				theme = "tokyonight",
-				component_separators = { left = "▒", right = "▒" },
-				section_separators = {},
-			},
-			sections = {
-				lualine_a = { { "mode", right_padding = 2 } },
-				lualine_b = { "diff", "diagnostics" },
-				lualine_c = { "harpoon2" },
-				lualine_x = {},
-				lualine_y = { "filetype", "progress" },
-				lualine_z = { { "location", left_padding = 2 } },
-			},
-			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = { "filename" },
-				lualine_x = {},
-				lualine_y = {},
-				lualine_z = { "location" },
-			},
-			tabline = {},
-			extensions = {},
-		},
-	},
-	{
-		"goolord/alpha-nvim",
-		lazy = false,
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function() require("alpha").setup(require("alpha.themes.dashboard").config) end,
 	},
 	{
 		"rcarriga/nvim-notify",
