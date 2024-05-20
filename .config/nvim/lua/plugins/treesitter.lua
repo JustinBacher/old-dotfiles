0c126808a1ad5e6c6dcf80879a068f5e78b91d91
JustinBacher <92jbach@gmail.com>
2024-05-19 21:50:47 -0400

    updating ts and making lazy

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,21 +1,50 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	version = "*",
-	event = "LazyFile",
+	version = false,
 	build = ":TSUpdate",
-	config = function()
-		--- @diagnostic disable-next-line: missing-fields
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
-			sync_install = false,
-			-- indent = { enable = true },
-			highlight = { enabled = false },
-			additional_vim_regex_highlighting = false,
-			disable = function(lang, buf)
-				local max_filesize = 100 * 1024 -- 100 KB
-				local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-				if ok and stats and stats.size > max_filesize then return true end
-			end,
-		})
+	event = "LazyFile",
+	lazy = vim.fn.argc(-1) == 0,
+	main = "nvim-treesitter.configs",
+	cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+	init = function(plugin)
+		require("lazy.core.loader").add_to_rtp(plugin)
+		require("nvim-treesitter.query_predicates")
 	end,
+	--- @diagnostic disable-next-line: missing-fields
+	opts = {
+		ensure_installed = {
+			"bash",
+			"c",
+			"diff",
+			"html",
+			"javascript",
+			"jsdoc",
+			"json",
+			"jsonc",
+			"lua",
+			"luadoc",
+			"luap",
+			"markdown",
+			"markdown_inline",
+			"python",
+			"query",
+			"regex",
+			"toml",
+			"tsx",
+			"typescript",
+			"vim",
+			"vimdoc",
+			"xml",
+			"yaml",
+		},
+		auto_install = true,
+		sync_install = true,
+		indent = { enable = true },
+		highlight = { enable = false },
+		disable = function(lang, buf) ---@diagnostic disable-line: unused-local
+			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+			return ok and stats and stats.size > 100 * 1024 -- 100kb
+		end,
+		additional_vim_regex_highlighting = false,
+	},
 }
