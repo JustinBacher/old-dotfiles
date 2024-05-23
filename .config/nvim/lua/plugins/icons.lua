6af990b75802f20183c8a235781f4449240f9ae3
Justin Bacher <32350542+JustinBacher@users.noreply.github.com>
2024-05-23 10:11:57 -0400

    Moved iconpicker to utils to avoid confusion withthe filenames

--- .config/nvim/lua/plugins/icons.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"ziontee113/icon-picker.nvim",
-	keys = {
-		{ "<leader>if", "<cmd>IconPickerNormal<cr>", desc = "Find Icon" },
-		{ "<leader>iy", "<cmd>IconPickerYank<cr>", desc = "Yank Icon" },
-	},
-	opts = { disable_legacy_commands = true },
-}
-
--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -85,4 +85,12 @@ return {
 		end,
 		config = true,
 	},
+	{
+		"ziontee113/icon-picker.nvim",
+		keys = {
+			{ "<leader>if", "<cmd>IconPickerNormal<cr>", desc = "Find Icon" },
+			{ "<leader>iy", "<cmd>IconPickerYank<cr>", desc = "Yank Icon" },
+		},
+		opts = { disable_legacy_commands = true },
+	}
 }
