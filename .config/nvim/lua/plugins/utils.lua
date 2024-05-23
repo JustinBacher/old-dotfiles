f9bbf8481917d3ecacd7536f6e538e1b4c8ed90c
Justin Bacher <32350542+JustinBacher@users.noreply.github.com>
2024-05-23 12:02:27 -0400

    Moved mini plugins to utils

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -1,5 +1,6 @@
 return {
 	{ "windwp/nvim-autopairs", config = true },
+	{ "echasnovski/mini.ai", event = "LazyFile", config = true },
 	{ "tenxsoydev/karen-yank.nvim", event = "LazyFile", config = true },
 	{
 		"Shatur/neovim-session-manager",
@@ -92,5 +93,23 @@ return {
 			{ "<leader>iy", "<cmd>IconPickerYank<cr>", desc = "Yank Icon" },
 		},
 		opts = { disable_legacy_commands = true },
-	}
+	},
+	{
+		"echasnovski/mini.surround",
+		event = "LazyFile",
+		opts = {
+			mappings = {
+				add = "gsa", -- Add surrounding in Normal and Visual modes
+				delete = "gsd", -- Delete surrounding
+				find = "gsf", -- Find surrounding (to the right)
+				find_left = "gsF", -- Find surrounding (to the left)
+				highlight = "gsh", -- Highlight surrounding
+				replace = "gsr", -- Replace surrounding
+				update_n_lines = "gsn", -- Update `n_lines`
+			},
+			custom_surroundings = {
+				["<"] = { output = { left = "<", right = ">" } },
+			},
+		},
+	},
 }
