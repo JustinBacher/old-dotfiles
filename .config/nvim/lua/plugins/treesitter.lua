b794dd78c9ce26b1b4812e1aff77cc40f453f2f3
Justin Bacher <32350542+JustinBacher@users.noreply.github.com>
2024-05-23 08:57:27 -0400

    Added context to the end of blocks, based on treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,63 +1,70 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	version = false,
-	event = "LazyFile",
-	lazy = vim.fn.argc(-1) == 0,
-	main = "nvim-treesitter.configs",
-	cmd = {
-		"TSUpdate",
-		"TSUpdateSync",
-		"TSUpdate",
-		"TSInstallInfo",
-		"TSInstall",
-		"TSEnable",
-		"TSDisable",
-		"TSBufEnable",
-		"TSBufDisable",
-		"TSModuleInfo",
-	},
-	init = function(plugin)
-		require("lazy.core.loader").add_to_rtp(plugin)
-		require("nvim-treesitter.query_predicates")
-		vim.filetype.add({ pattern = { [".*/hypr/.*%.conf"] = "hyprlang" } })
-	end,
-	build = ":TSUpdate",
-	--- @diagnostic disable-next-line: missing-fields
-	opts = {
-		ensure_installed = {
-			"bash",
-			"c",
-			"diff",
-			"html",
-			"hyprlang",
-			"javascript",
-			"jsdoc",
-			"json",
-			"jsonc",
-			"lua",
-			"luadoc",
-			"luap",
-			"markdown",
-			"markdown_inline",
-			"python",
-			"query",
-			"regex",
-			"toml",
-			"tsx",
-			"typescript",
-			"vim",
-			"vimdoc",
-			"xml",
-			"yaml",
+ {
+		"andersevenrud/nvim_context_vt",
+  event = "LazyFile",
+	 config = true
+ },
+ {
+		"nvim-treesitter/nvim-treesitter",
+		version = false,
+		event = "LazyFile",
+		lazy = vim.fn.argc(-1) == 0,
+		main = "nvim-treesitter.configs",
+		cmd = {
+			"TSUpdate",
+			"TSUpdateSync",
+			"TSUpdate",
+			"TSInstallInfo",
+			"TSInstall",
+			"TSEnable",
+			"TSDisable",
+			"TSBufEnable",
+			"TSBufDisable",
+			"TSModuleInfo",
 		},
-		auto_install = true,
-		sync_install = true,
-		indent = { enable = true },
-		highlight = { enable = false },
-		disable = function(lang, buf) ---@diagnostic disable-line: unused-local
-			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-			return ok and stats and stats.size > 100 * 1024 -- 100kb
+		init = function(plugin)
+			require("lazy.core.loader").add_to_rtp(plugin)
+			require("nvim-treesitter.query_predicates")
+			vim.filetype.add({ pattern = { [".*/hypr/.*%.conf"] = "hyprlang" } })
 		end,
-		additional_vim_regex_highlighting = false,
-	},
+		build = ":TSUpdate",
+		--- @diagnostic disable-next-line: missing-fields
+		opts = {
+			ensure_installed = {
+				"bash",
+				"c",
+				"diff",
+				"html",
+				"hyprlang",
+				"javascript",
+				"jsdoc",
+				"json",
+				"jsonc",
+				"lua",
+				"luadoc",
+				"luap",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"toml",
+				"tsx",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"xml",
+				"yaml",
+			},
+			auto_install = true,
+			sync_install = true,
+			indent = { enable = true },
+			highlight = { enable = false },
+			disable = function(lang, buf) ---@diagnostic disable-line: unused-local
+				local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+				return ok and stats and stats.size > 100 * 1024 -- 100kb
+			end,
+			additional_vim_regex_highlighting = false,
+		},
+ },
 }
