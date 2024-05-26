06da343146f8988cf45f7879d1fb993a8086c155
JustinBacher <92jbach@gmail.com>
2024-05-26 18:50:24 -0400

    got rid of ai completions fully

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -70,12 +70,11 @@ return {
 			local luasnip = require("luasnip")
 			local lspkind = require("lspkind")
 			local icons = require("plugins.configs.icons").lsp
-			local compare = require("cmp.config.compare")
+			-- local compare = require("cmp.config.compare")
 
 			cmp.setup({
 				enabled = true,
 				experimental = { ghost_text = true },
-				---@diagnostic disable-next-line: missing-fields
 				snippet = { expand = function(args) require("luasnip").lsp_expand(args.body) end },
 				completion = { keyword_length = 1 },
 				preselect = cmp.PreselectMode.Item,
@@ -97,20 +96,20 @@ return {
 				}, {
 					{ name = "buffer", keyword_length = 2, max_item_count = 5, group_index = 2 },
 				}),
-				sorting = {
-					priority_weight = 2,
-					-- comparators = {
-					-- 	require("cmp_ai.compare"),
-					-- 	compare.offset,
-					-- 	compare.exact,
-					-- 	compare.score,
-					-- 	compare.recently_used,
-					-- 	compare.kind,
-					-- 	compare.sort_text,
-					-- 	compare.length,
-					-- 	compare.order,
-					-- },
-				},
+				-- sorting = {
+				-- 	priority_weight = 2,
+				-- 	comparators = {
+				-- 		require("cmp_ai.compare"),
+				-- 		compare.offset,
+				-- 		compare.exact,
+				-- 		compare.score,
+				-- 		compare.recently_used,
+				-- 		compare.kind,
+				-- 		compare.sort_text,
+				-- 		compare.length,
+				-- 		compare.order,
+				-- 	},
+				-- },
 				---@diagnostic disable-next-line: missing-fields
 				formatting = {
 					fields = { "kind", "abbr", "menu" },
@@ -141,7 +140,6 @@ return {
 						border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
 						winhighlight = "Normal:Normal,FloatBorder:LspBorderBG,CursorLine:PmenuSel,Search:None", -- "Normal:Normal,Search:None",
 						col_offset = 0,
-						side_padding = 0,
 					},
 					documentation = cmp.config.window.bordered({
 						winhighlight = "Normal:Normal,FloatBorder:LspBorderBG,CursorLine:PmenuSel,Search:None",
@@ -177,44 +175,37 @@ return {
 			if presentAutopairs then
 				cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
 			end
-			vim.api.nvim_set_hl(0, "PmenuSel", { bg = "NONE", fg = "NONE" })
-			vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { fg = "#7E8294", bg = "NONE", strikethrough = true })
-			vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#82AAFF", bg = "NONE", bold = true })
-			vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#82AAFF", bg = "NONE", bold = true })
-			vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#C792EA", bg = "NONE", italic = true })
-
-			vim.api.nvim_set_hl(0, "CmpItemKindField", { fg = "#EED8DA", bg = "#B5585F" })
-			vim.api.nvim_set_hl(0, "CmpItemKindProperty", { fg = "#EED8DA", bg = "#B5585F" })
-			vim.api.nvim_set_hl(0, "CmpItemKindEvent", { fg = "#EED8DA", bg = "#B5585F" })
-
-			vim.api.nvim_set_hl(0, "CmpItemKindText", { fg = "#C3E88D", bg = "#9FBD73" })
-			vim.api.nvim_set_hl(0, "CmpItemKindEnum", { fg = "#C3E88D", bg = "#9FBD73" })
-			vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { fg = "#C3E88D", bg = "#9FBD73" })
-
-			vim.api.nvim_set_hl(0, "CmpItemKindConstant", { fg = "#FFE082", bg = "#D4BB6C" })
-			vim.api.nvim_set_hl(0, "CmpItemKindConstructor", { fg = "#FFE082", bg = "#D4BB6C" })
-			vim.api.nvim_set_hl(0, "CmpItemKindReference", { fg = "#FFE082", bg = "#D4BB6C" })
-
-			vim.api.nvim_set_hl(0, "CmpItemKindFunction", { fg = "#EADFF0", bg = "#A377BF" })
-			vim.api.nvim_set_hl(0, "CmpItemKindStruct", { fg = "#EADFF0", bg = "#A377BF" })
-			vim.api.nvim_set_hl(0, "CmpItemKindClass", { fg = "#EADFF0", bg = "#A377BF" })
-			vim.api.nvim_set_hl(0, "CmpItemKindModule", { fg = "#EADFF0", bg = "#A377BF" })
-			vim.api.nvim_set_hl(0, "CmpItemKindOperator", { fg = "#EADFF0", bg = "#A377BF" })
-
-			vim.api.nvim_set_hl(0, "CmpItemKindVariable", { fg = "#C5CDD9", bg = "#7E8294" })
-			vim.api.nvim_set_hl(0, "CmpItemKindFile", { fg = "#C5CDD9", bg = "#7E8294" })
-
-			vim.api.nvim_set_hl(0, "CmpItemKindUnit", { fg = "#F5EBD9", bg = "#D4A959" })
-			vim.api.nvim_set_hl(0, "CmpItemKindSnippet", { fg = "#F5EBD9", bg = "#D4A959" })
-			vim.api.nvim_set_hl(0, "CmpItemKindFolder", { fg = "#F5EBD9", bg = "#D4A959" })
-
-			vim.api.nvim_set_hl(0, "CmpItemKindMethod", { fg = "#DDE5F5", bg = "#6C8ED4" })
-			vim.api.nvim_set_hl(0, "CmpItemKindValue", { fg = "#DDE5F5", bg = "#6C8ED4" })
-			vim.api.nvim_set_hl(0, "CmpItemKindEnumMember", { fg = "#DDE5F5", bg = "#6C8ED4" })
-
-			vim.api.nvim_set_hl(0, "CmpItemKindInterface", { fg = "#D8EEEB", bg = "#58B5A8" })
-			vim.api.nvim_set_hl(0, "CmpItemKindColor", { fg = "#D8EEEB", bg = "#58B5A8" })
-			vim.api.nvim_set_hl(0, "CmpItemKindTypeParameter", { fg = "#D8EEEB", bg = "#58B5A8" })
+			local hl = vim.api.nvim_set_hl
+			hl(0, "PmenuSel", { bg = "NONE", fg = "NONE" })
+			hl(0, "CmpItemAbbrDeprecated", { fg = "#7E8294", bg = "NONE", strikethrough = true })
+			hl(0, "CmpItemAbbrMatch", { fg = "#82AAFF", bg = "NONE", bold = true })
+			hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#82AAFF", bg = "NONE", bold = true })
+			hl(0, "CmpItemMenu", { fg = "#C792EA", bg = "NONE", italic = true })
+			hl(0, "CmpItemKindField", { fg = "#EED8DA", bg = "#B5585F" })
+			hl(0, "CmpItemKindProperty", { fg = "#EED8DA", bg = "#B5585F" })
+			hl(0, "CmpItemKindEvent", { fg = "#EED8DA", bg = "#B5585F" })
+			hl(0, "CmpItemKindText", { fg = "#C3E88D", bg = "#9FBD73" })
+			hl(0, "CmpItemKindEnum", { fg = "#C3E88D", bg = "#9FBD73" })
+			hl(0, "CmpItemKindKeyword", { fg = "#C3E88D", bg = "#9FBD73" })
+			hl(0, "CmpItemKindConstant", { fg = "#FFE082", bg = "#D4BB6C" })
+			hl(0, "CmpItemKindConstructor", { fg = "#FFE082", bg = "#D4BB6C" })
+			hl(0, "CmpItemKindReference", { fg = "#FFE082", bg = "#D4BB6C" })
+			hl(0, "CmpItemKindFunction", { fg = "#EADFF0", bg = "#A377BF" })
+			hl(0, "CmpItemKindStruct", { fg = "#EADFF0", bg = "#A377BF" })
+			hl(0, "CmpItemKindClass", { fg = "#EADFF0", bg = "#A377BF" })
+			hl(0, "CmpItemKindModule", { fg = "#EADFF0", bg = "#A377BF" })
+			hl(0, "CmpItemKindOperator", { fg = "#EADFF0", bg = "#A377BF" })
+			hl(0, "CmpItemKindVariable", { fg = "#C5CDD9", bg = "#7E8294" })
+			hl(0, "CmpItemKindFile", { fg = "#C5CDD9", bg = "#7E8294" })
+			hl(0, "CmpItemKindUnit", { fg = "#F5EBD9", bg = "#D4A959" })
+			hl(0, "CmpItemKindSnippet", { fg = "#F5EBD9", bg = "#D4A959" })
+			hl(0, "CmpItemKindFolder", { fg = "#F5EBD9", bg = "#D4A959" })
+			hl(0, "CmpItemKindMethod", { fg = "#DDE5F5", bg = "#6C8ED4" })
+			hl(0, "CmpItemKindValue", { fg = "#DDE5F5", bg = "#6C8ED4" })
+			hl(0, "CmpItemKindEnumMember", { fg = "#DDE5F5", bg = "#6C8ED4" })
+			hl(0, "CmpItemKindInterface", { fg = "#D8EEEB", bg = "#58B5A8" })
+			hl(0, "CmpItemKindColor", { fg = "#D8EEEB", bg = "#58B5A8" })
+			hl(0, "CmpItemKindTypeParameter", { fg = "#D8EEEB", bg = "#58B5A8" })
 		end,
 	},
 }
