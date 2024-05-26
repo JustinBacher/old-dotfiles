a918c457432ee4530569c7e7cb4b9832094b5c12
JustinBacher <92jbach@gmail.com>
2024-05-26 19:47:18 -0400

    change the colors of command parameters

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,16 +5,16 @@ if status is-interactive
     	exec tmux 
 end
 
-set -l foreground c8d3f5
-set -l selection 2d3f76
-set -l comment 636da6
-set -l red ff757f
-set -l orange ff966c
-set -l yellow ffc777
-set -l green c3e88d
-set -l purple fca7ea
-set -l cyan 86e1fc
-set -l pink c099ff
+set -l foreground c8d3f5 #c8d3f5
+set -l selection 2d3f76 #2d3f76
+set -l comment 636da6 #636da6
+set -l red ff757f #ff757f
+set -l orange ff966c #ff966c
+set -l yellow ffc777 #ffc777
+set -l green c3e88d #c3e88d
+set -l purple fca7ea #fca7ea
+set -l cyan 86e1fc #86e1fc
+set -l pink c099ff #c099ff
 
 # Syntax Highlighting Colors
 set -g fish_color_normal $foreground
@@ -24,7 +24,7 @@ set -g fish_color_quote $yellow
 set -g fish_color_redirection $foreground
 set -g fish_color_end $orange
 set -g fish_color_error $red
-set -g fish_color_param $purple
+set -g fish_color_param $pink
 set -g fish_color_comment $comment
 set -g fish_color_selection --background=$selection
 set -g fish_color_search_match --background=$selection
