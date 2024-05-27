265b8c7f71965f0e6c3c112a8e9e6e6983965024
JustinBacher <92jbach@gmail.com>
2024-05-27 14:39:13 -0400

    Added keymap to commit current buffer

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,8 +1,21 @@
+local function git_commit_current_buffer()
+	vim.ui.input({prompt = "Message"}, function (message)
+		if message == "" then print("No commit performed") end
+		-- local path = vim.split(vim.fn.expand('%'), "/")
+		-- local filename = table.remove(path, #path)
+		vim.cmd("Git add " .. vim.fn.expand('%'))
+		vim.cmd("Git commit  --message=" .. '"' .. message .. '"')
+
+		-- vim.cmd("Git commit " .. message)
+	end)
+end
 return {
 	{
 		"tpope/vim-fugitive",
-		keys = {},
-		config = true,
+		cmd = "Git",
+		keys = {
+			{ "<leader>gc", git_commit_current_buffer, desc = "Git add current file" },
+		},
 	},
 	{
 		"lewis6991/gitsigns.nvim",
@@ -20,9 +33,7 @@ return {
 			"LazyGitFilter",
 			"LazyGitFilterCurrentFile",
 		},
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
+		dependencies = "nvim-lua/plenary.nvim",
 		keys = {
 			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
 		},
