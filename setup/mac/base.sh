86fdc92d2152a2fb223405a96b88ad87ee192c54
Justin Bacher <32350542+JustinBacher@users.noreply.github.com>
2024-07-20 14:35:48 -0400

    Make install script for mac

---
+++ setup/mac/base.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+read -r -p "This script will install/update Homebrew and some nessecary applications to your system. Do you wish to continue? [y/N] " response
+response=${response,,}
+if [[ ! $REPLY =~ ^[Yy]$ ]] then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
+
+# check if brew is installed and install if not, otherwise make sure it is up to date
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    . <"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    brew update
+fi
+
+brew install gum
